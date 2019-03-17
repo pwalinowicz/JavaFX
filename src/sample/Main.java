5c21b322ff3c1e0bdea2908fd802566bd7f3f23e
Pawel <pejo9@op.pl>
2019-03-17 17:20:27 +0100

    Changing GridPane from fxml files

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,20 +16,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setVgap(10);
-        root.setHgap(10);
-
-        Label greeting = new Label("Welcome to JavaFX");
-        greeting.setTextFill(Color.GREEN);
-        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
-        root.getChildren().add(greeting);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 700, 275));
+        primaryStage.setScene(new Scene(root, 500, 275));
         primaryStage.show();
     }
 
